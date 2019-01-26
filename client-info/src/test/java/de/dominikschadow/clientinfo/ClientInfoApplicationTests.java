ad2f2e57fbfbc737e67ad864dca8655117e5a13d
Dominik Schadow <dominikschadow@gmail.com>
2019-01-26 13:46:09 +0100

    Initial version

---
+++ client-info/src/test/java/de/dominikschadow/clientinfo/ClientInfoApplicationTests.java
@@ -0,0 +1,17 @@
+package de.dominikschadow.clientinfo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClientInfoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
