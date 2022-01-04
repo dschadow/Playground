6d2e58dcde0f0bce7da25007d6eec5143f1ba1e0
Dominik Schadow <mail@dominikschadow.de>
2022-01-04 16:15:12 +0100

    fixed test

--- client-info/src/test/java/de/dominikschadow/clientinfo/ClientInfoApplicationTests.java
+++ client-info/src/test/java/de/dominikschadow/clientinfo/ClientInfoApplicationTests.java
@@ -1,17 +1,12 @@
 package de.dominikschadow.clientinfo;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class ClientInfoApplicationTests {
-
     @Test
     public void contextLoads() {
     }
-
 }
 
